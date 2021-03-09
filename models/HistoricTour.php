c68a460c1938de5629e27b9c211312b33a4bd037
Cyberduc-k <tttymo@gmail.com>
2021-03-09 15:55:10 +0100

    add HistoricTour model

---
+++ models/HistoricTour.php
@@ -0,0 +1,42 @@
+<?php
+
+class HistoricTour {
+    private int $id;
+    private String $guide;
+    private DateTime $date;
+    private int $language;
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function setId(int $id): void {
+        $this->id = $id;
+    }
+
+    public function getGuide(): String {
+        return $this->guide;
+    }
+
+    public function setGuide(String $guide): void {
+        $this->guide = $guide;
+    }
+
+    public function getDate(): DateTime {
+        return $this->date;
+    }
+
+    public function setDate(DateTime $date): void {
+        $this->date = $date;
+    }
+
+    public function getLanguage(): int {
+        return $this->language;
+    }
+
+    public function setLanguage(int $language): void {
+        $this->language = $language;
+    }
+}
+
+?>
---
+++ models/Language.php
@@ -0,0 +1,8 @@
+<?php
+
+abstract class Language {
+    public const Dutch   = 0;
+    public const English = 1;
+}
+
+?>
