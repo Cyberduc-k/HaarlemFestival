b0fa3715e0b0a38a72e5205eb59e4b7695dcd36a
Tim850 <timkompier850@gmail.com>
2021-03-10 12:27:45 +0100

    foodType numbers

--- models/FoodType.php
+++ models/FoodType.php
@@ -2,10 +2,10 @@
 
 abstract class FoodType {
     public const French = 0;
-    public const Italian = 0;
-    public const Dutch = 0;
-    public const Mexican = 0;
-    public const Asian = 0;
+    public const Italian = 1;
+    public const Dutch = 2;
+    public const Mexican = 3;
+    public const Asian = 4;
 }
 
 ?>
