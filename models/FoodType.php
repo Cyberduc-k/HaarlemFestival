9c8e0bd25c5847c939f8db6b2591c1fe17dd62c1
Tim850 <timkompier850@gmail.com>
2021-03-10 12:23:14 +0100

    Add a few more FoodTypes

--- models/FoodType.php
+++ models/FoodType.php
@@ -2,6 +2,10 @@
 
 abstract class FoodType {
     public const French = 0;
+    public const Italian = 0;
+    public const Dutch = 0;
+    public const Mexican = 0;
+    public const Asian = 0;
 }
 
 ?>
