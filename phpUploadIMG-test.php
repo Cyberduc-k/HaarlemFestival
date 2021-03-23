cfca4861532b7fe29639d7deb1b9065baad4ea2d
Tim850 <timkompier850@gmail.com>
2021-03-23 15:41:22 +0100

    Deleted upload test

--- phpUploadIMG-test.php
+++
@@ -1,5 +0,0 @@
-<form action="uploadIMG.php" method="post" enctype="multipart/form-data">
-    Select File to Upload:
-    <input type="file" name="file">
-    <input type="submit" name="submit" value="Upload" required>
-</form>
