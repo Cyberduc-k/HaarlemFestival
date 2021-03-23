f1afd75978248d5d882ca8dce3a7c38834f63aa6
Tim850 <timkompier850@gmail.com>
2021-03-23 15:24:18 +0100

    Upload picture test in food page gezet

--- foodPage.php
+++ foodPage.php
@@ -103,6 +103,15 @@
         <header>
             <h3>
                 Restaurants
+
+                Upload picture:
+
+                <form action="uploadIMG.php" method="post" enctype="multipart/form-data">
+                    Select File to Upload:
+                    <input type="file" name="file">
+                    <input type="submit" name="submit" value="Upload" required>
+                </form>
+
             </h3>
         </header>
 
