93815a13693b39af5716b03274b43c9f2cc7d22a
InvaderCipher <55230649+InvaderCipher@users.noreply.github.com>
2021-04-05 16:11:01 +0200

    simple acount page start

---
+++ account.php
@@ -0,0 +1,23 @@
+<?php
+if(!isset($_SESSION)) session_start();
+?>
+<html lang="en">
+    <head>
+        <title>Home</title>
+        <link type="text/css" rel="stylesheet" href="css/style.css" />
+    </head>
+    <body>
+    <?php
+
+    require_once("menubar.php");
+    require_once("services/UserService.php");
+
+    $userService = new UserService();
+
+    echo "<h1 style='float: left'>Welcome " . $_SESSION['firstname'] . "!</h1>";
+    echo "<img class='avatar' src=".$userService->getAvatarByEmail($_SESSION["email"])." alt='avatar' style='float: left; margin: 40px 0px 0px 40px'>";
+
+    ?>
+    </body>
+</html>
+
