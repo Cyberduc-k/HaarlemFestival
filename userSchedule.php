c65e012898a43a019cbb5a4d590f3f0ff08b17b1
Cyberduc-k <tttymo@gmail.com>
2021-04-07 22:10:54 +0200

    improve userSchedule page

--- userSchedule.php
+++ userSchedule.php
@@ -23,118 +23,90 @@ if (!isset($_SESSION["userType"])) {
             <thead>
                 <tr>
                     <th></th>
-                    <th colspan="4">09:00</th>
-                    <th colspan="4">11:00</th>
-                    <th colspan="4">13:00</th>
-                    <th colspan="4">15:00</th>
-                    <th colspan="4">17:00</th>
-                    <th colspan="4">19:00</th>
-                    <th colspan="4">21:00</th>
-                    <th colspan="4">23:00</th>
-                    <th colspan="4">01:00</th>
+                    <th>09:00</th>
+                    <th>11:00</th>
+                    <th>13:00</th>
+                    <th>15:00</th>
+                    <th>17:00</th>
+                    <th>19:00</th>
+                    <th>21:00</th>
+                    <th>23:00</th>
+                    <th>01:00</th>
                 </tr>
             </thead>
             <tbody>
-                <?php
-                    require_once("services/TicketService.php");
-
-                    $ticketService = new TicketService();
-                    $tickets = $ticketService->getAllForUser((int)$_SESSION["userId"]);
-                    $thursday = '<tr><td>Thursday</td>';
-                    $friday = '<tr><td>Friday</td>';
-                    $saturday = '<tr><td>Saturday</td>';
-                    $sunday = '<tr><td>Sunday</td>';
-
-                    $tickets_thursday = [];
-                    $tickets_friday = [];
-                    $tickets_saturday = [];
-                    $tickets_sunday = [];
-
-                    if ($tickets == null) {
-                        $tickets = [];
-                    }
-
-                    foreach ($tickets as $twc) {
-                        $ticket = $twc->ticket;
-                        $start = $ticketService->getStartDate($ticket);
-                        $end = $ticketService->getEndDate($ticket);
-                        $date = (int)$start->format('d');
-
-                        switch ($date) {
-                            case 26:
-                                array_push($tickets_thursday, ['ticket' => $ticket, 'start' => $start, 'end' => $end]);
-                                break;
-                            case 27:
-                                array_push($tickets_friday, ['ticket' => $ticket, 'start' => $start, 'end' => $end]);
-                                break;
-                            case 28:
-                                array_push($tickets_saturday, ['ticket' => $ticket, 'start' => $start, 'end' => $end]);
-                                break;
-                            default:
-                                array_push($tickets_sunday, ['ticket' => $ticket, 'start' => $start, 'end' => $end]);
-                                break;
-                        }
-                    }
-
-                    $sort_fn = function(array $a, array $b): int {
-                        if ($a['start'] == $b['start']) {
-                            return 0;
-                        } else if ($a['start'] < $b['start']) {
-                            return -1;
-                        } else {
-                            return 1;
-                        }
-                    };
-
-                    usort($tickets_thursday, $sort_fn);
-                    usort($tickets_friday, $sort_fn);
-                    usort($tickets_saturday, $sort_fn);
-                    usort($tickets_sunday, $sort_fn);
-
-                    function timeDiff(DateTime $start, DateTime $end): int {
-                        $start2 = new DateTime($start->format('H:i:s'));
-                        $end2 = new DateTime($end->format('H:i:s'));
-
-                        if ($start2 > $end2) {
-                            $end2->add(new DateInterval('P1D'));
-                        }
-
-                        $diff = $start2->diff($end2);
-                        $res = $diff->i / 30;
-                        $res += $diff->h * 2;
-
-                        return $res;
-                    }
-
-                    function processDay(string &$text, array $tickets) {
-                        $time = new DateTime('08:00:00');
-
-                        foreach ($tickets as $tkt) {
-                            for ($i = timeDiff($time, $tkt['start']); $i > 0; $i--) {
-                                $text .= '<td></td>';
-                            }
-
-                            $diff = timeDiff($tkt['start'], $tkt['end']);
-                            if ($diff > 0) $text .= '<td colspan="' . $diff . '"></td>';
-                            $time = $tkt['end'];
-                        }
-
-                        for ($i = timeDiff($time, new DateTime('02:00:00')); $i > 0; $i--) {
-                            $text .= '<td></td>';
-                        }
-                    }
-
-                    processDay($thursday, $tickets_thursday);
-                    processDay($friday, $tickets_friday);
-                    processDay($saturday, $tickets_saturday);
-                    processDay($sunday, $tickets_sunday);
-
-                    echo $thursday; echo '</td>';
-                    echo $friday; echo '</td>';
-                    echo $saturday; echo '</td>';
-                    echo $sunday; echo '</td>';
-                ?>
+                <tr>
+                    <td>Thursday</td>
+                    <td colspan="9"></td>
+                </tr>
+                <tr>
+                    <td>Friday</td>
+                    <td colspan="9"></td>
+                </tr>
+                <tr>
+                    <td>Saturday</td>
+                    <td colspan="9"></td>
+                </tr>
+                <tr>
+                    <td>Sunday</td>
+                    <td colspan="9"></td>
+                </tr>
             </tbody>
+            <?php
+                require_once("services/TicketService.php");
+                require_once("models/EventType.php");
+
+                $ticketService = new TicketService();
+                $tickets = $ticketService->getAllForUser((int)$_SESSION["userId"]);
+
+                $tickets_thursday = [];
+                $tickets_friday = [];
+                $tickets_saturday = [];
+                $tickets_sunday = [];
+
+                if ($tickets == null) {
+                    $tickets = [];
+                }
+
+                $tickets = array_map(function($twc) use ($ticketService) {
+                    $ticket = $twc->ticket;
+                    $start = $ticketService->getStartDate($ticket);
+                    $end = $ticketService->getEndDate($ticket);
+
+                    return ['ticket' => $ticket, 'start' => $start, 'end' => $end];
+                }, $tickets);
+
+                function timeDiff(DateTime $start, DateTime $end): int {
+                    $start2 = new DateTime($start->format('H:i:s'));
+                    $end2 = new DateTime($end->format('H:i:s'));
+
+                    if ($start2 > $end2) {
+                        $end2->add(new DateInterval('P1D'));
+                    }
+
+                    $diff = $start2->diff($end2);
+                    $res = $diff->i / 30;
+                    $res += $diff->h * 2;
+
+                    return $res;
+                }
+
+                foreach ($tickets as $tkt) {
+                    $diff = timeDiff($tkt['start'], $tkt['end']);
+
+                    if ($diff > 0) {
+                        $date = (int)$tkt['start']->format("d") - 26;
+                        $end = new DateTime('02:00:00');
+                        $end = timeDiff($tkt['end'], $end);
+                        $event = EventType::getType($tkt['ticket']->getEventType());
+                        $name = $ticketService->getDescription($tkt['ticket']);
+
+                        echo '<div class="event ' . $event . '"';
+                        echo ' style="--end: ' . $end . '; --day: ' . $date . '; --width: ' . $diff . ';">';
+                        echo $name . '</div>';
+                    }
+                }
+            ?>
         </table>
     </main>
 </body>
