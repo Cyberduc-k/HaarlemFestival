27f9f81659e2d379dcdbb12e77b3319c89891e40
InvaderCipher <55230649+InvaderCipher@users.noreply.github.com>
2021-04-07 19:21:23 +0200

    moved edit info, edit pofile, programme to account

--- account.php
+++ account.php
@@ -5,6 +5,7 @@ if(!isset($_SESSION)) session_start();
     <head>
         <title>Account</title>
         <link type="text/css" rel="stylesheet" href="css/style.css" />
+        <link type="text/css" rel="stylesheet" href="css/account.css" />
     </head>
     <body>
     <?php
@@ -14,6 +15,23 @@ if(!isset($_SESSION)) session_start();
 
     $userService = new UserService();
 
+    ?>
+
+    <nav>
+        <ul>
+            <li>
+                <a href='userSchedule.php'>Programme</a>
+            </li>
+            <li>
+                <a href='edit.php?type=own'>Edit my information</a>
+            </li>
+            <li>
+                <a href='changeAvatar.php'>Change Avatar</a>
+            </li>
+        </ul>
+    </nav>
+
+    <?php
     echo "<h1 style='float: left'>Welcome " . $_SESSION['firstname'] . "!</h1>";
     echo "<img class='avatar' src=".$userService->getAvatarByEmail($_SESSION["email"])." alt='avatar' style='float: left; margin: 40px 0px 0px 40px'>";
 
