9006f36a01ed49ea3d54d8e84b820967e281eb52
Tim850 <timkompier850@gmail.com>
2021-04-08 17:45:34 +0200

    added addRestaurant

--- addRestaurant.php
+++ addRestaurant.php
@@ -1,7 +1,18 @@
 <?php
+require_once ("services/RestaurantService.php");
+require_once ("models/Restaurant.php");
 
+$rs = new RestaurantService;
 
-class addRestaurant
-{
+$name = $_POST["name"];
+$location = $_POST["location"];
+$foodType = $_POST["foodType"];
 
-}
+$restaurant = new Restaurant();
+$restaurant->setName($name);
+$restaurant->setLocation($location);
+$restaurant->setFoodType($foodType);
+$rs->addRestaurant($restaurant);
+
+?>
+
