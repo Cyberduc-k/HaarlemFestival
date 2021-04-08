cb9c1b0a8cbb7e94b0226fdd8e60fa7562a43fdf
Cyberduc-k <tttymo@gmail.com>
2021-04-08 19:50:10 +0200

    prettier footer

--- footer.php
+++ footer.php
@@ -1,12 +1,16 @@
-<?php
-
-echo '<footer id="foot">';
-echo '<ul><li><a href="" class="icon instagram"></a></li><li><a href="" class="icon facebook"></a></li></ul>';
-echo '<ul>
+<footer id="foot">
+    <ul>
+        <li>
+            <a href="" class="icon instagram"></a>
+        </li>
+        <li>
+            <a href="" class="icon facebook"></a>
+        </li>
+    </ul>
+    <ul>
         <li>Contact</li>
         <li>Privacy Policy</li>
         <li>Terms and Conditions</li>
-        </ul>';
-echo '<p id="copyright"> &copy;Haarlem Festival </p>';
-echo '</footer>';
-?>
+    </ul>
+    <p id="copyright"> &copy;Haarlem Festival </p>
+</footer>
